81447769f12416a4658d3bebf345e77a4807d5c3
Mathis Bois <mathis.bois@outlook.fr>
2022-09-17 13:53:43 +0200

    [Dev] models: improve note model

--- models/note.model.go
+++ models/note.model.go
@@ -1,6 +1,10 @@
 package models
 
 type Note struct {
-	Message string
-	Tag     string
+	Message string `json:"message"`
+	Tag     string `json:"tag"`
+}
+
+func (n *Note) IsTaggedNote() bool {
+	return len(n.Tag) > 0
 }
