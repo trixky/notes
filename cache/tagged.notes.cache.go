901bca4e19dcf62a8e4ff8e0fdce2cccd421c66b
Mathis Bois <mathis.bois@outlook.fr>
2022-09-19 16:43:28 +0200

    [Comments] cache: add comments for caches

--- cache/tagged.notes.cache.go
+++ cache/tagged.notes.cache.go
@@ -6,29 +6,38 @@ import (
 	"github.com/trixky/tt_orness/models"
 )
 
+// taggedNotesCache saves tagged notes in the cache (not persistent).
 type taggedNotesCache struct {
 	mtx   sync.RWMutex
 	Notes map[string][]models.Note
 }
 
+// Add adds a tagged note in the cache.
 func (tn *taggedNotesCache) Add(note models.Note) {
 	tn.mtx.Lock()
 
 	if notes, ok := tn.Notes[note.Tag]; ok {
+		// If the tag is already known
+		// Add the note with others associated notes.
 		tn.Notes[note.Tag] = append(notes, note)
 	} else {
+		// If the tag is new
+		// Create a new group for notes.
 		tn.Notes[note.Tag] = []models.Note{note}
 	}
 
 	tn.mtx.Unlock()
 }
 
+// Get gets tagged notes in the cache for a specific tag.
 func (tn *taggedNotesCache) Get(tag string) []models.Note {
 	tn.mtx.RLock()
 
+	// Get note for this tag.
 	notes, ok := tn.Notes[tag]
 
 	if !ok {
+		// If no known note for this tag.
 		notes = []models.Note{}
 	}
 
@@ -37,12 +46,15 @@ func (tn *taggedNotesCache) Get(tag string) []models.Note {
 	return notes
 }
 
+// GetAll gets all tagged notes in the cache.
 func (tn *taggedNotesCache) GetAll() []models.Note {
 	tn.mtx.RLock()
 
 	notes := []models.Note{}
 
 	for _, tagged_notes := range tn.Notes {
+		// For each tag known in the cache
+		// Get all notes associated with this tag.
 		notes = append(notes, tagged_notes...)
 	}
 
@@ -51,11 +63,15 @@ func (tn *taggedNotesCache) GetAll() []models.Note {
 	return notes
 }
 
+// Delete deletes tagged notes in the cache for a specific tag.
 func (tn *taggedNotesCache) Delete(tag string) (number_of_deleted_notes int) {
 	tn.mtx.Lock()
 
 	if notes, ok := tn.Notes[tag]; ok {
+		// If the tag is known.
+		// Get the number of notes that will be deleted.
 		number_of_deleted_notes = len(notes)
+		// Delete all notes associated with this tag.
 		delete(tn.Notes, tag)
 	}
 
@@ -64,11 +80,15 @@ func (tn *taggedNotesCache) Delete(tag string) (number_of_deleted_notes int) {
 	return
 }
 
+// DeleteAll deletes all tagged notes in the cache.
 func (tn *taggedNotesCache) DeleteAll() (number_of_deleted_notes int) {
 	tn.mtx.Lock()
 
 	for tag := range tn.Notes {
+		// For each tag known in the cache.
+		// Get the number of notes that will be deleted.
 		number_of_deleted_notes += len(tn.Notes[tag])
+		// Delete all notes associated with this tag.
 		delete(tn.Notes, tag)
 	}
 
@@ -77,4 +97,5 @@ func (tn *taggedNotesCache) DeleteAll() (number_of_deleted_notes int) {
 	return
 }
 
+// Init the global "tagged notes" cache.
 var TaggedNotes = taggedNotesCache{Notes: make(map[string][]models.Note)}
--- cache/untagged.notes.cache.go
+++ cache/untagged.notes.cache.go
@@ -6,22 +6,27 @@ import (
 	"github.com/trixky/tt_orness/models"
 )
 
+// untaggedNotesCache saves untagged notes in the cache (not persistent).
 type untaggedNotesCache struct {
 	mtx   sync.RWMutex
 	Notes []models.Note
 }
 
+// Add adds untagged a note in the cache.
 func (un *untaggedNotesCache) Add(note models.Note) {
 	un.mtx.Lock()
 
+	// Add the note.
 	un.Notes = append(un.Notes, note)
 
 	un.mtx.Unlock()
 }
 
+// Get gets all untagged notes in the cache.
 func (un *untaggedNotesCache) Get() []models.Note {
 	un.mtx.RLock()
 
+	// Get all notes.
 	notes := un.Notes
 
 	un.mtx.RUnlock()
@@ -29,10 +34,13 @@ func (un *untaggedNotesCache) Get() []models.Note {
 	return notes
 }
 
+// DeleteAll deletes all untagged notes in the cache.
 func (un *untaggedNotesCache) DeleteAll() (number_of_deleted_notes int) {
 	un.mtx.Lock()
 
+	// Get the number of notes that will be deleted.
 	number_of_deleted_notes = len(un.Notes)
+	// Delete all notes.
 	un.Notes = nil
 
 	un.mtx.Unlock()
@@ -40,4 +48,5 @@ func (un *untaggedNotesCache) DeleteAll() (number_of_deleted_notes int) {
 	return
 }
 
+// Init the global "untagged notes" cache.
 var UntaggedNotes = untaggedNotesCache{Notes: []models.Note{}}
