a4577aaf7209355808f4d0b7b84f10a5413b1dde
Mathis Bois <mathis.bois@outlook.fr>
2022-09-19 16:06:46 +0200

    [Dev] env: put the environments variable names in the config

---
+++ config/environment.go
@@ -0,0 +1,3 @@
+package config
+
+const ENV_NOTE_PORT = "NOTE_PORT"
--- main.go
+++ main.go
@@ -9,16 +9,14 @@ import (
 	"github.com/trixky/tt_orness/services"
 )
 
-const port_env = "NOTE_PORT"
-
 var port = config.DEFAULT_PORT
 
 func init() {
-	readed_port := os.Getenv(port_env)
+	readed_port := os.Getenv(config.ENV_NOTE_PORT)
 
 	if len(readed_port) > 0 {
 		if new_port, err := strconv.Atoi(readed_port); err != nil {
-			log.Fatalln(port_env + " environment variable is corrupted: " + err.Error())
+			log.Fatalln(config.ENV_NOTE_PORT + " environment variable is corrupted: " + err.Error())
 		} else {
 			port = new_port
 		}
